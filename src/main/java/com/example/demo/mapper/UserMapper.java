1105b98bea13c1ef5dd752c41569654eb6ef7cb6
neiranie <email_anda@contoh.com>
2026-03-03 15:50:49 +0700

    Adding UserService

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,4 +1,12 @@
 package com.example.demo.mapper;
 
+import com.example.demo.mapper.dto.UserDto;
+import com.example.demo.model.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+    UserMapper MAPPER = Mappers.getMapper(UserMapper.class);
+    UserDto toUserDtoData (User, user);
 }
